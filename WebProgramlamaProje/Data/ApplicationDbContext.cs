7d5efc0d271f12e91b00c204370587835dca2d55
Zey <zeynepirem2034@hotmail.com>
2024-12-03 13:56:22 +0300

    Veritabanı bağlantısı yapıldı.

---
+++ WebProgramlamaProje/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
+
+public class User
+{
+    public int UserId { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Role { get; set; }
+}
--- WebProgramlamaProje/Program.cs
+++ WebProgramlamaProje/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebProgramlamaProje
 {
     public class Program
@@ -8,6 +10,9 @@ namespace WebProgramlamaProje
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
