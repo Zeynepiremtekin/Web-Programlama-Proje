0a9eb41c97f6c63f03cbd0e2257c312dda7ed5b0
busra-kaya <busrakayacs@gmail.com>
2024-12-15 20:02:34 +0300

    AboutUs sayfa tasarımları düzenlendi.

--- WebProgramlamaProje/Controllers/HomeController.cs
+++ WebProgramlamaProje/Controllers/HomeController.cs
@@ -32,6 +32,15 @@ namespace WebProgramlamaProje.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
